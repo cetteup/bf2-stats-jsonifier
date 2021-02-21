7a377e4a339fb6a03d0ab07654c7aeaed4eb33a5
cetteup <me@cetteup.com>
2021-02-21 23:03:53 +0100

    Added support for PlayBF2 aka other revive project

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const fetch = require('node-fetch');
 
-const baseUrl = 'http://official.ranking.bf2hub.com/ASP/';
+const projects = {
+    bf2hub: {
+        baseUrl: 'http://official.ranking.bf2hub.com/ASP/',
+        defaultHeaders: {
+            'User-Agent': 'GameSpyHTTP/1.0',
+            Host: 'BF2web.gamespy.com'
+        }
+    },
+    playbf2: {
+        baseUrl: 'http://bf2web.playbf2.ru/ASP/',
+        defaultHeaders: {
+            'User-Agent': 'GameSpyHTTP/1.0'
+        }
+    }
+};
+
 const sources = {
     getplayerinfo: {
         endpoint: 'getplayerinfo.aspx',
@@ -34,7 +49,7 @@ const sources = {
         propertyKeys: ['players'],
         forceReturnArray: true
     }
-}
+};
 
 exports.lambdaHandler = async (event) => {
     // Init response
@@ -53,7 +68,7 @@ exports.lambdaHandler = async (event) => {
         }
 
         // Make sure required query params have been provided
-        const requiredParamsPresent = sources[sourceKey].requiredParams.every((param) => Object.keys(event.queryStringParameters).includes(param) && event.queryStringParameters[param].trim().length > 0)
+        const requiredParamsPresent = sources[sourceKey].requiredParams.every((param) => Object.keys(event.queryStringParameters).includes(param) && event.queryStringParameters[param].trim().length > 0);
         if (!event.queryStringParameters || !requiredParamsPresent) {
             response.statusCode = 422;
             throw new Error('Missing required query string paramter(s)');
@@ -65,14 +80,17 @@ exports.lambdaHandler = async (event) => {
             throw new Error('searchforplayers does not support "endswith"/"where=e" search');
         }
 
-        const stats = await fetchFromSource(sources[sourceKey], event.queryStringParameters);
+        // Determine BF2 "revive" project to request data from (defaulting to BF2Hub)
+        const project = event?.queryStringParameters?.project in projects ? projects[event.queryStringParameters.project] : projects.bf2hub;
+
+        const stats = await fetchFromSource(project, sources[sourceKey], event.queryStringParameters);
 
         // Finish setting up response
         response.statusCode = 200;
-        response.headers['Cache-Control'] = `max-age=${process.env.CACHE_TTL || 600}`
+        response.headers['Cache-Control'] = `max-age=${process.env.CACHE_TTL || 600}`;
         response.body = JSON.stringify(stats);
     } catch (err) {
-        console.log(err)
+        console.log(err);
         if (err.message === 'Player not found') {
             response.statusCode = 404;
         }
@@ -84,22 +102,19 @@ exports.lambdaHandler = async (event) => {
     return response;
 };
 
-async function fetchFromSource(source, eventQueryParameters) {
+async function fetchFromSource(project, source, eventQueryParameters) {
     let response;
     const queryParams = { ...source.defaultParams, ...eventQueryParameters };
-    const url = new URL(source.endpoint + '?' + Object.entries(queryParams).map((param) => `${param[0]}=${param[1]}`).join('&'), baseUrl);
+    const url = new URL(source.endpoint + '?' + Object.entries(queryParams).map((param) => `${param[0]}=${param[1]}`).join('&'), project.baseUrl);
     response = await fetch(url, {
-        headers: {
-            'User-Agent': 'GameSpyHTTP/1.0',
-            Host: 'BF2web.gamespy.com'
-        }
+        headers: project.defaultHeaders
     });
 
     // Parse BF2 data format
     const rawResponse = await response.text();
     const parsedResponse = await parseBf2Response(rawResponse, source.propertyKeys, source.forceReturnArray);
 
-    return parsedResponse
+    return parsedResponse;
 }
 
 async function parseBf2Response(rawResponse, propertyKeys, forceReturnArray = false) {
@@ -108,7 +123,7 @@ async function parseBf2Response(rawResponse, propertyKeys, forceReturnArray = fa
 
     // Make sure first line indicates ok status
     const firstLine = lines.shift();
-    if (!firstLine.startsWith('O')) {
+    if (firstLine.trim() != 'O') {
         // Throw specific error if player was not found, else use generic message
         const errMsg = firstLine == 'E\t998' ? 'Player not found' : 'Source query resulted in an error';
         throw new Error(errMsg);
@@ -120,22 +135,22 @@ async function parseBf2Response(rawResponse, propertyKeys, forceReturnArray = fa
     let dataLineIndex = 0;
     lines.forEach((line) => {
         switch (line.substr(0, 2)) {
-            case 'H\t':
-                lastDelimiterType = 'h';
-                datasets.push({ h: line.substr(2), d: [] });
-                break;
-            case 'D\t':
-                // If a data is followed by a data row, copy headers and create a new entry
-                if (lastDelimiterType === 'd') {
-                    dataLineIndex++;
-                }
-                lastDelimiterType = 'd';
-                datasets[datasets.length - 1].d[dataLineIndex] = line.substr(2);
-                break;
-            case '$\t':
-                break;
-            default:
-                datasets[datasets.length - 1][lastDelimiterType] += line;
+        case 'H\t':
+            lastDelimiterType = 'h';
+            datasets.push({ h: line.substr(2), d: [] });
+            break;
+        case 'D\t':
+            // If a data is followed by a data row, copy headers and create a new entry
+            if (lastDelimiterType === 'd') {
+                dataLineIndex++;
+            }
+            lastDelimiterType = 'd';
+            datasets[datasets.length - 1].d[dataLineIndex] = line.substr(2);
+            break;
+        case '$\t':
+            break;
+        default:
+            datasets[datasets.length - 1][lastDelimiterType] += line;
         }
     });
 
@@ -160,7 +175,7 @@ async function parseBf2Response(rawResponse, propertyKeys, forceReturnArray = fa
         // Multiple lines of data, create array of objects
         else {
             const propertyKey = propertyKeys[datasetIndex - 1];
-            returnObj[propertyKey] = dataLines.map((line) => Object.fromEntries(line.map((value, index) => [keys[index], value])))
+            returnObj[propertyKey] = dataLines.map((line) => Object.fromEntries(line.map((value, index) => [keys[index], value])));
         }
     });
 
